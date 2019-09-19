fec4403ab70a64cfdfaa53fe4e4635d878f25ecd
Guilherme Pontes <gui97p.p@gmail.com>
2019-09-18 23:56:06 -0300

    Adjusting parameters on exercise 5

--- python/exercise5.py
+++ python/exercise5.py
@@ -2,14 +2,14 @@ from neuralNetwork.Adaline import Adaline
 from common.adaline_exercises_functions import pre_processing, generate_random_cases
 
 X, y = pre_processing()
-X_val, y_val = generate_random_cases()
+X_val, y_val = generate_random_cases(10)
 
 perceptron = Adaline([X], [y], eta=0.001, epochs=100000,
                      uses_batch=False, error_threshold=0.00000000001,
                      validatin_X=X_val, validation_y=y_val)
 
 
-X_test, y_test = generate_random_cases()
+X_test, y_test = generate_random_cases(10)
 
 for x_row, y in zip(X_test, y_test):
     perceptron.predict(x_row)
