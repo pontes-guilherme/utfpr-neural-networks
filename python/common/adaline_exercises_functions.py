443bb76a12b38193eaa52254525596a58bf08df1
Guilherme Pontes <gui97p.p@gmail.com>
2019-09-18 22:53:17 -0300

    Added common functions for adaline exercises

---
+++ python/common/adaline_exercises_functions.py
@@ -0,0 +1,32 @@
+from math import pi, sin, cos
+
+
+def f1(z):
+    return sin(z)
+
+
+def f2(z):
+    return cos(z)
+
+
+def f3(z):
+    return z
+
+
+def f(z):
+    return -pi + 0.565*f1(z) + 2.657*f2(z) + 0.674*f3(z)
+
+
+def pre_processing(size=15):
+    step = (2*pi)/size
+
+    X = []
+    y = []
+
+    n = 0
+    while n <= 2*pi:
+        X.append([f1(n), f2(n), f3(n)])
+        y.append(f(n))
+        n += step
+
+    return X, y
